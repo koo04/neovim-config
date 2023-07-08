c4f7908b2943f2d345df18936c3e07bc88d58a7b
Douglas DeVicariis <douglas@devicariis.org>
2023-07-07 20:55:29 -0400

    Added lazygit to the installer

--- install.sh
+++ install.sh
@@ -15,6 +15,12 @@ sudo apt-get install -y fd-find ripgrep fzf python3-pip
 # install neovim from snap
 sudo snap install nvim --classic
 
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm lazygit.tar.gz
+
 # check if the $XDG_CONFIG_HOME/nvim directory exists
 if [ ! -d "$XDG_CONFIG_HOME/nvim" ]; then
   mkdir -p $XDG_CONFIG_HOME/nvim
