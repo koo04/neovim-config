76528cbb3f77c5b7805a3c1896a93ca117061e67
koo04 <douglas@devicariis.org>
2024-02-09 14:57:44 -0500

    remove local share on install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # check if the symbolic link exists already
 if [ -L $HOME/.config/nvim ]; then
-    rm $HOME/.config/nvim
+    rm -rf $HOME/.config/nvim
+    rm -rf $HOME/.local/share/nvim
 fi
 
 ln -s $(pwd) $HOME/.config/nvim
