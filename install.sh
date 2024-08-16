4355ad1a4f0af78c4991abea4d2746ca3acd2b3e
koo04 <douglas@devicariis.org>
2024-08-16 19:42:16 -0400

    Fixed

--- install.sh
+++ install.sh
@@ -17,14 +17,14 @@ curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/
 tar xf /tmp/lazygit.tar.gz -C /tmp lazygit
 sudo install /tmp/lazygit /usr/local/bin
 
-# clone the repo
-if [ ! -d $HOME/.config/nvim ]; then
-    git clone git@github.com:koo04/neovim-config $HOME/.config/nvim
-fi
-
 # check if the symbolic link exists already
 if [ -L $HOME/.config/nvim ]; then
     rm -rf $HOME/.config/nvim $HOME/.local/share/nvim $HOME/.local/state/nvim $HOME/.cache/nvim
+fi
+
+# clone the repo
+if [ ! -d $HOME/.config/nvim ]; then
+    git clone git@github.com:koo04/neovim-config $HOME/.config/nvim
 fi
 
 ln -s $(pwd) $HOME/.config/nvim
