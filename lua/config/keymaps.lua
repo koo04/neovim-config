261a84558f40f9a12b7e2d51c34e06fb2dff9e57
Douglas.DeVicariis <douglas.devicariis@prizepicks.com>
2024-12-03 16:01:42 -0500

    Lots of fixes and improvements

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<leader>1", "1gt", { desc = "Move to tab 1" })
+map("n", "<leader>2", "2gt", { desc = "Move to tab 2" })
+map("n", "<leader>3", "3gt", { desc = "Move to tab 3" })
+map("n", "<leader>4", "4gt", { desc = "Move to tab 4" })
+map("n", "<leader>5", "5gt", { desc = "Move to tab 5" })
+map("n", "<leader>6", "6gt", { desc = "Move to tab 6" })
+map("n", "<leader>7", "7gt", { desc = "Move to tab 7" })
+map("n", "<leader>8", "8gt", { desc = "Move to tab 8" })
+map("n", "<leader>9", "9gt", { desc = "Move to tab 9" })
